91b9f3cb4320d7187c8e9f7950a63fcd98f7bd22
cruzcordero <cruzcordero7600@gmail.com>
2022-07-14 15:03:16 -0700

    reset java code and using a new URL

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,82 +1,40 @@
-//Get value input 
+//Get value input
+
+let searchBtn = document.querySelector(".searchBtn");
+
+function getCityData() {
+    cityName = document.querySelector(".search_city_input").value;
+    let apiKey = `51cc602a695be172598f1b878becb16e`;
+
+    fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
+    )
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            for (let i = 0; i < data.length; i++) {
+                let cityName = document.querySelector(".city_name");
+                cityName.textContent = data[i].name;
+                console.log(cityName);
+            }
+        });
+}
+
+searchBtn.addEventListener("click", getCityData);
 //Api call for the city based on the city name and not latitude and longitude
 
-let searchBtn = document.querySelector('.searchBtn');
-
-
-function citySearch () {
-    let cityStateName = document.querySelector('.search_city_input').value;
-    let APICallCity = `http://api.openweathermap.org/geo/1.0/direct?q=${cityStateName}&appid=51cc602a695be172598f1b878becb16e`;
-    let ApiCallCityData = `https://api.openweathermap.org/data/2.5/onecall?lat=${this.getLatitude}&lon=${this.getLongitude}&units=imperial&appid=51cc602a695be172598f1b878becb16e`
-    
-    
-    fetch(APICallCity)
-        .then( response => {
-            return response.json()
-        })
-        .then(function(data) {
-            console.log(data);
-            for(let i = 0; i < data.length; i++) {
-                let cityName = document.querySelector('.city_name')
-                    cityName.textContent = data[i].local_names.en + ', ' + data[i].state;
-
-                let cityLog = document.querySelector('.logged-cities')
-                let pastCityName = document.createElement('button')
-                    pastCityName.classList.add('past-searched-city')
-                    pastCityName.textContent = data[i].local_names.en;
-                    cityLog.appendChild(pastCityName);
-                let currentDay = document.querySelector('.current_date');        
-                let getLatitude = data[i].lat;
-                    console.log(getLatitude)
-                let getLongitude = data[i].lon
-                    console.log(getLongitude)
-
-                } 
-    let ApiCallCityData = `https://api.openweathermap.org/data/2.5/onecall?lat=${this.getLatitude}&lon=${this.getLongitude}&units=imperial&appid=51cc602a695be172598f1b878becb16e`
-    fetch(ApiCallCityData)
-    .then( response => {
-        return response.json()
-    })
-    .then(function(weather) {
-        console.log(weather);
-        for(let i = 0; i < weather.length; i++) {
-            let tempatureCurrentDay =  document.querySelector('.current_tempature_insert').textContent = weather[i].daily.temp.day;
-                console.log(tempatureCurrentDay)
-            }
-        })
-        
-        }
-        )
-
-}
-    //search button corresponding to getting city data
-    searchBtn.addEventListener('click', citySearch)
+//search button corresponding to getting city data
 
 //converting city name to long and lat
 
-
-
-    //log current date and the next five days
-
+//log current date and the next five days
 
 //logg tempature for crrent date and 5 future days
 
 //logg Wind for crrent date and 5 future days
 
-
 //logg humidity for crrent date and 5 future days
 
-
 //logg UV index for crrent date and 5 future days
-
-
-
-
-
-
-
-
-
-
-
-
