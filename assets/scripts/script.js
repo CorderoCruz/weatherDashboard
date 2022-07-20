5f08ddffedb8e170546ef966d473ab051f3bec50
cruzcordero <cruzcordero7600@gmail.com>
2022-07-19 22:26:45 -0700

    generated a button with the city name to the logged cities children to get local storage late on

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,6 +2,11 @@ const searchBtn = document.querySelector(".searchBtn");
 
 const getData = function () {
     const cityNameInput = document.querySelector(".search_city_input").value;
+    const pastCitiesLog = document.querySelector(".logged_cities");
+    const pastCities = document.createElement("button");
+    pastCities.textContent = cityNameInput.toUpperCase();
+    pastCities.classList.add("past_searched_city");
+    pastCitiesLog.appendChild(pastCities);
     this.cityURL =
         cityURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityNameInput}&appid=51cc602a695be172598f1b878becb16e`;
 
@@ -19,8 +24,8 @@ const getData = function () {
                 document.querySelector(".city_name").textContent =
                     data[i].local_names.en + ", " + data[i].state;
                 //eliminating the latitude and longitude to only have two numbers after the decimal
-                let latitude = Math.round(latLong.latitude * 100) / 100;
-                let longitude = Math.round(latLong.longitude * 100) / 100;
+                const latitude = Math.round(latLong.latitude * 100) / 100;
+                const longitude = Math.round(latLong.longitude * 100) / 100;
                 function convertData() {
                     let cityLatLongUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&appid=51cc602a695be172598f1b878becb16e`;
                     fetch(cityLatLongUrl)
@@ -105,32 +110,32 @@ const getData = function () {
                             let dayFourTemp = document.querySelector(
                                 ".forecast_four_temp_insert"
                             );
-                            dayFourTemp.textContent = `${data.daily[1].temp.day} \u2109`;
+                            dayFourTemp.textContent = `${data.daily[3].temp.day} \u2109`;
                             //log day one wind
                             let dayFourWind = document.querySelector(
                                 ".forecast_four_wind_insert"
                             );
-                            dayFourWind.textContent = `${data.daily[1].wind_speed} mph`;
+                            dayFourWind.textContent = `${data.daily[3].wind_speed} mph`;
                             //log day one humidy
                             let dayFourHum = document.querySelector(
                                 ".forecast_four_humidity_insert"
                             );
-                            dayFourHum.textContent = `${data.daily[1].humidity} %`;
+                            dayFourHum.textContent = `${data.daily[3].humidity} %`;
                             //----------------LOGS FOR DAY FIVE ----------------------------
                             let dayFiveTemp = document.querySelector(
                                 ".forecast_five_temp_insert"
                             );
-                            dayFiveTemp.textContent = `${data.daily[1].temp.day} \u2109`;
+                            dayFiveTemp.textContent = `${data.daily[4].temp.day} \u2109`;
                             //log day one wind
                             let dayFiveWind = document.querySelector(
                                 ".forecast_five_wind_insert"
                             );
-                            dayFiveWind.textContent = `${data.daily[1].wind_speed} mph`;
+                            dayFiveWind.textContent = `${data.daily[4].wind_speed} mph`;
                             //log day one humidy
                             let dayFiveHum = document.querySelector(
                                 ".forecast_five_humidity_insert"
                             );
-                            dayFiveHum.textContent = `${data.daily[1].humidity} %`;
+                            dayFiveHum.textContent = `${data.daily[4].humidity} %`;
                         });
                 }
                 convertData();
